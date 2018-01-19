075cd8447b5dff59802ecda955f453ac6f78995e
lihe <lihe12@baidu.com>
2018-01-19 18:41:35 +0800

    增加LR代码

--- LR.py
+++ LR.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 import pandas as pd
-import statsmodels.api as am
+# import statsmodels.api as am
 import pylab as pl
 import numpy as np
 
@@ -12,10 +12,17 @@ import numpy as np
 df = pd.read_csv('D:/scikit-learnDemos/lr-binary.csv')
 print(df.head())
 df.columns = ['admit', 'gre', 'gpa', 'prestige']
-# 使用 pandas 的函数 describe 来给出数据的摘要–describe
-print(df.describe())
-print(df.std())
-#频率表，表示 prestige 与 admin 的值相应的数量关系
-print(pd.crosstab(df['admit'],df['prestige'],rownames=['admit']))
-df.hist()
-pl.show()
+# # 使用 pandas 的函数 describe 来给出数据的摘要–describe
+# print(df.describe())
+# print(df.std())
+# #频率表，表示 prestige 与 admin 的值相应的数量关系
+# print(pd.crosstab(df['admit'],df['prestige'],rownames=['admit']))
+# df.hist()
+# pl.show()
+dummy_ranks = pd.get_dummies(df['prestige'],prefix='prestige')
+print(dummy_ranks.head())
+cols_to_keep = ['admit','gre','gpa']
+data = df[cols_to_keep].join(dummy_ranks.ix[:,'prestige_2':])
+print(data.head())
+# 需要自行添加逻辑回归所需的 intercept 变量
+data['intercept'] = 1.0
